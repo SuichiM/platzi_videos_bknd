424a041284145d7c2fedd920a9469dd68f55fcb6
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-03-29 07:43:59 -0300

    added the basic auth strategy

---
+++ src/movies-api/utils/auth/strategies/basic.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+
+const UserService = require('../../../services/users');
+
+passport.use(
+  new BasicStrategy(async function (email, pass, cb) {
+    const userService = new UserService();
+
+    try {
+      const user = userService.getUser({ email });
+      if (!user) return cb(boom.unauthorized(), false);
+
+      if (!(await bcrypt.compare(pass, user.password)))
+        return cb(boom.unauthorized(), false);
+
+      delete user.password;
+
+      return cb(false, user);
+    } catch (error) {
+      cb(error);
+    }
+  })
+);
