64f4bb990064257422aa6a418922e7c0c49e4ee2
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-03-29 08:34:36 -0300

    added jwt strategy

---
+++ src/movies-api/utils/auth/strategies/jwt.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-http');
+const boom = require('@hapi/boom');
+
+const UserService = require('../../../services/users');
+const { config } = require('../../../config');
+
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: config.auth_jwt_secret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async function (tokenPayload, cb) {
+      const userService = new UserService();
+
+      try {
+        const user = userService.getUser({ user: tokenPayload.email });
+        if (!user) return cb(boom.unauthorized(), false);
+
+        delete user.password;
+
+        return cb(false, user);
+      } catch (error) {}
+    }
+  )
+);
