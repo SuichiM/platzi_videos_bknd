0432b294115a06c18718aaf27edc78813939f700
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-03-29 07:28:05 -0300

    scripts to seed the DB

--- src/movies-api/config/index.js
+++ src/movies-api/config/index.js
@@ -9,7 +9,12 @@ const config = {
   db_name: process.env.DB_NAME,
   db_user: process.env.DB_USER,
   db_pass: process.env.DB_PASS,
-  db_protocol: process.env.DB_PROTOCOL
+  db_protocol: process.env.DB_PROTOCOL,
+  default_admin_password: process.env.DEFAULT_ADMIN_PASSWORD,
+  default_user_password: process.env.DEFAULT_USER_PASSWORD,
+  auth_jwt_secret: process.env.AUTH_JWT_SECRET,
+  public_api_key_token: process.env.PUBLIC_API_KEY_TOKEN,
+  admin_api_key_token: process.env.ADMIN_API_KEY_TOKEN,
 };
 
 module.exports = { config };
