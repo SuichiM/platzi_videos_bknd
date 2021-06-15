b869ff8b9e2a42a8ec89c42b7e1343dcf6cc58b9
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-06-15 11:23:24 -0300

    added the basic auth strategy on the SSR server

--- src/ssr-server/index.js
+++ src/ssr-server/index.js
@@ -1,18 +1,66 @@
 const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
+
+const util = require("util");
+
 
 const { config } = require("./config");
-
 const app = express();
 
-// body parser
+// middlewares
 app.use(express.json());
+app.use(cookieParser());
+
+/* Auth strategies */
+require('./utils/auth/strategies/basic');
+
 
 app.post("/auth/sign-in", async function(req, res, next) {
+  
+  passport.authenticate("basic", function(error, data){
+    try {
+  
+      if(error || ! data)
+        return next(boom.unauthorized())
+      
+      req.login(data, {session:false}, async function(error){
+        if(error)
+          return next(error)
+        
+        const { token, ...user } = data;
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        })
+        
+        res.status(200).json(user);
 
+      })
+
+
+    } catch (error) {
+      next(error)
+    }
+  })(req, res, next)
 });
 
 app.post("/auth/sign-up", async function(req, res, next) {
+  const { body: user } = req;
 
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.get("/movies", async function(req, res, next) {
